aa998f610352b0a3ddfddb65b9aa11a7427b0293
xmq <123@qq.com>
2021-01-29 12:35:12 +0800

    张三第二次提交了代码

--- 01-java基础测试/src/com/gec/demotest/DemomTest1.java
+++ 01-java基础测试/src/com/gec/demotest/DemomTest1.java
@@ -2,6 +2,11 @@ package com.gec.demotest;
 
 public class DemomTest1 {
 
+
+    public void aa()
+    {
+        System.out.println("aa 方法..");
+    }
     public static void main(String[] args) {
 
 
