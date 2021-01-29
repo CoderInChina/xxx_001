0f77c73fc0db9cb1750de5204381c73f9dd32090
xmq <123@qq.com>
2021-01-29 13:53:30 +0800

    111

--- 01-java基础测试/src/com/gec/demotest/DemomTest1.java
+++ 01-java基础测试/src/com/gec/demotest/DemomTest1.java
@@ -5,7 +5,7 @@ public class DemomTest1 {
 
     public void aa()
     {
-        System.out.println("aa 方法aaaa..");
+        System.out.println("aa 方法aaaa..111111");
     }
     public static void main(String[] args) {
 
